e751d6debb3764656426d8551310082149f507e1
yamarus <ruslan712@gmail.com>
2020-11-16 12:39:45 +0700

    add tasks in theme7

---
+++ source/lectures/t7_diff.py
@@ -0,0 +1,51 @@
+from matplotlib import rcParams
+rcParams['font.family'] = 'serif'
+rcParams['font.serif'] = ['Times New Roman']
+rcParams["figure.dpi"]=600
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+data=np.loadtxt('t5_diff.csv',delimiter=', ',skiprows=1)
+w,v = np.linalg.eig(np.cov(data.T))
+
+line1=np.array([[v[:,0][0],-v[:,0][0]],[v[:,0][1],-v[:,0][1]]])*w[0]**0.5*2
+line2=np.array([[v[:,1][0],-v[:,1][0]],[v[:,1][1],-v[:,1][1]]])*w[1]**0.5*2
+
+plt.subplot(222)
+bins=np.linspace(-80,80,51)
+plt.hist(data[:,0],bins,color='red')
+plt.ylim(0,60)
+ax=plt.gca()
+plt.xticks(np.linspace(-75,75,7))
+ax.set_aspect(1)
+plt.text(50,45,r'$x^{fin}, \AA$')
+plt.xlabel(r'$\Delta x, \AA$')
+
+plt.subplot(224)
+
+plt.hist(data[:,1],bins,color='black')
+plt.text(50,45,r'$y^{fin}, \AA$')
+plt.ylim(0,60)
+ax=plt.gca()
+ax.set_aspect(1)
+plt.xticks(np.linspace(-75,75,7))
+plt.xlabel(r'$\Delta y, \AA$')
+
+plt.subplots_adjust(hspace=-0.2)
+
+plt.subplot(121)
+plt.plot(data[:,0],data[:,1], 'g.',markersize=1)
+plt.plot(line1[0],line1[1],'r-',linewidth=2, label=r'$D_1$')
+plt.plot(line2[0],line2[1],'k-',linewidth=2, label=r'$D_2$')
+plt.xticks(np.linspace(-75,75,7))
+plt.xlim(-80,80)
+plt.ylim(-80,80)
+ax=plt.gca()
+ax.set_aspect(1.)
+plt.legend(loc='upper right')
+plt.xlabel(r'$\Delta x, \AA$')
+plt.ylabel(r'$\Delta y, \AA$')
+
+plt.savefig('t7_diff.pdf')
+plt.show()
